a5278a4ccdde02a0391a80877307312267054921
jeter1011 <erickagudelo123@gmail.com>
2021-08-22 18:07:51 -0400

    ml practice

--- Exercise3/e3p1.py
+++ Exercise3/e3p1.py
@@ -6,8 +6,10 @@ from matplotlib import pyplot
 import numpy as np
 # will be used to load MATLAB mat datafile format
 from scipy.io import loadmat
+import scipy
 
-# library written for this exercise providing additional functions for assignment submission, and others
+from scipy import optimize
+
 import utils
 
 # Plotting library
@@ -154,10 +156,279 @@ def lrCostFunction(theta, X, y, lambda_):
 
 J, grad = lrCostFunction(theta_t, X_t, y_t, lambda_t)
 
-print('Cost         : {:.6f}'.format(J))
-print('Expected cost: 2.534819')
-print('-----------------------')
-print('Gradients:')
-print(' [{:.6f}, {:.6f}, {:.6f}, {:.6f}]'.format(*grad))
-print('Expected gradients:')
-print(' [0.146561, -0.548558, 0.724722, 1.398003]');
+# print('Cost         : {:.6f}'.format(J))
+# print('Expected cost: 2.534819')
+# print('-----------------------')
+# print('Gradients:')
+# print(' [{:.6f}, {:.6f}, {:.6f}, {:.6f}]'.format(*grad))
+# print('Expected gradients:')
+# print(' [0.146561, -0.548558, 0.724722, 1.398003]');
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+def oneVsAll(X, y, num_labels, lambda_):
+    """
+    Trains num_labels logistic regression classifiers and returns
+    each of these classifiers in a matrix all_theta, where the i-th
+    row of all_theta corresponds to the classifier for label i.
+
+    Parameters
+    ----------
+    X : array_like
+        The input dataset of shape (m x n). m is the number of
+        data points, and n is the number of features. Note that we
+        do not assume that the intercept term (or bias) is in X, however
+        we provide the code below to add the bias term to X.
+
+    y : array_like
+        The data labels. A vector of shape (m, ).
+
+    num_labels : int
+        Number of possible labels.
+
+    lambda_ : float
+        The logistic regularization parameter.
+
+    Returns
+    -------
+    all_theta : array_like
+        The trained parameters for logistic regression for each class.
+        This is a matrix of shape (K x n+1) where K is number of classes
+        (ie. `numlabels`) and n is number of features without the bias.
+
+    Instructions
+    ------------
+    You should complete the following code to train `num_labels`
+    logistic regression classifiers with regularization parameter `lambda_`.
+
+    Hint
+    ----
+    You can use y == c to obtain a vector of 1's and 0's that tell you
+    whether the ground truth is true/false for this class.
+
+    Note
+    ----
+    For this assignment, we recommend using `scipy.optimize.minimize(method='CG')`
+    to optimize the cost function. It is okay to use a for-loop
+    (`for c in range(num_labels):`) to loop over the different classes.
+
+    Example Code
+    ------------
+
+        # Set Initial theta
+        initial_theta = np.zeros(n + 1)
+
+        # Set options for minimize
+        options = {'maxiter': 50}
+
+        # Run minimize to obtain the optimal theta. This function will
+        # return a class object where theta is in `res.x` and cost in `res.fun`
+        res = optimize.minimize(lrCostFunction,
+                                initial_theta,
+                                (X, (y == c), lambda_),
+                                jac=True,
+                                method='TNC',
+                                options=options)
+    """
+    # Some useful variables
+    m, n = X.shape
+
+    # You need to return the following variables correctly
+    all_theta = np.zeros((num_labels, n + 1))
+
+    # Add ones to the X data matrix
+    X = np.concatenate([np.ones((m, 1)), X], axis=1)
+
+    # ====================== YOUR CODE HERE ======================
+    for c in range(num_labels):
+
+        initial_theta = np.zeros(n + 1)
+        options = {'maxiter': 50}
+        res = optimize.minimize(lrCostFunction,
+                                initial_theta,
+                                (X, (y == c), lambda_),
+                                jac=True,
+                                method='TNC',
+                                options=options)
+
+        all_theta[c] = res.x
+
+
+    # ============================================================
+    return all_theta
+
+lambda_ = 0.1
+all_theta= oneVsAll(X, y, num_labels, lambda_)
+
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+
+def predictOneVsAll(all_theta, X):
+    """
+    Return a vector of predictions for each example in the matrix X.
+    Note that X contains the examples in rows. all_theta is a matrix where
+    the i-th row is a trained logistic regression theta vector for the
+    i-th class. You should set p to a vector of values from 0..K-1
+    (e.g., p = [0, 2, 0, 1] predicts classes 0, 2, 0, 1 for 4 examples) .
+
+    Parameters
+    ----------
+    all_theta : array_like
+        The trained parameters for logistic regression for each class.
+        This is a matrix of shape (K x n+1) where K is number of classes
+        and n is number of features without the bias.
+
+    X : array_like
+        Data points to predict their labels. This is a matrix of shape
+        (m x n) where m is number of data points to predict, and n is number
+        of features without the bias term. Note we add the bias term for X in
+        this function.
+
+    Returns
+    -------
+    p : array_like
+        The predictions for each data point in X. This is a vector of shape (m, ).
+
+    Instructions
+    ------------
+    Complete the following code to make predictions using your learned logistic
+    regression parameters (one-vs-all). You should set p to a vector of predictions
+    (from 0 to num_labels-1).
+
+    Hint
+    ----
+    This code can be done all vectorized using the numpy argmax function.
+    In particular, the argmax function returns the index of the max element,
+    for more information see '?np.argmax' or search online. If your examples
+    are in rows, then, you can use np.argmax(A, axis=1) to obtain the index
+    of the max for each row.
+    """
+    m = X.shape[0];
+    num_labels = all_theta.shape[0]
+
+    # You need to return the following variables correctly
+    p = np.zeros(m)
+
+    # Add ones to the X data matrix
+    X = np.concatenate([np.ones((m, 1)), X], axis=1)
+    x1 = X[0:1]
+    y1 = y[0:1]
+
+
+
+
+    # ====================== YOUR CODE HERE ======================
+    h = utils.sigmoid(np.dot(X, np.transpose(all_theta)))
+
+    p = np.argmax(h,axis=1)
+    # ============================================================
+    return p
+
+#  training data stored in arrays X, y
+data = loadmat(os.path.join('Data', 'ex3data1.mat'))
+X, y = data['X'], data['y'].ravel()
+
+# set the zero digit to 0, rather than its mapped 10 in this dataset
+# This is an artifact due to the fact that this dataset was used in
+# MATLAB where there is no index 0
+y[y == 10] = 0
+
+# get number of examples in dataset
+m = y.size
+
+# randomly permute examples, to be used for visualizing one
+# picture at a time
+indices = np.random.permutation(m)
+
+# Randomly select 100 data points to display
+rand_indices = np.random.choice(m, 100, replace=False)
+sel = X[rand_indices, :]
+
+utils.displayData(sel)
+#-----------------------------------------------------------------------------------------------------------------------
+# Setup the parameters you will use for this exercise
+input_layer_size  = 400  # 20x20 Input Images of Digits
+hidden_layer_size = 25   # 25 hidden units
+num_labels = 10          # 10 labels, from 0 to 9
+
+# Load the .mat file, which returns a dictionary
+weights = loadmat(os.path.join('Data', 'ex3weights.mat'))
+
+# get the model weights from the dictionary
+# Theta1 has size 25 x 401
+# Theta2 has size 10 x 26
+Theta1, Theta2 = weights['Theta1'], weights['Theta2']
+
+# swap first and last columns of Theta2, due to legacy from MATLAB indexing,
+# since the weight file ex3weights.mat was saved based on MATLAB indexing
+Theta2 = np.roll(Theta2, 1, axis=0)
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+
+def predict(Theta1, Theta2, X):
+    """
+    Predict the label of an input given a trained neural network.
+
+    Parameters
+    ----------
+    Theta1 : array_like
+        Weights for the first layer in the neural network.
+        It has shape (2nd hidden layer size x input size)
+
+    Theta2: array_like
+        Weights for the second layer in the neural network.
+        It has shape (output layer size x 2nd hidden layer size)
+
+    X : array_like
+        The image inputs having shape (number of examples x image dimensions).
+
+    Return
+    ------
+    p : array_like
+        Predictions vector containing the predicted label for each example.
+        It has a length equal to the number of examples.
+
+    Instructions
+    ------------
+    Complete the following code to make predictions using your learned neural
+    network. You should set p to a vector containing labels
+    between 0 to (num_labels-1).
+
+    Hint
+    ----
+    This code can be done all vectorized using the numpy argmax function.
+    In particular, the argmax function returns the index of the  max element,
+    for more information see '?np.argmax' or search online. If your examples
+    are in rows, then, you can use np.argmax(A, axis=1) to obtain the index
+    of the max for each row.
+
+    Note
+    ----
+    Remember, we have supplied the `sigmoid` function in the `utils.py` file.
+    You can use this function by calling `utils.sigmoid(z)`, where you can
+    replace `z` by the required input variable to sigmoid.
+    """
+    # Make sure the input has two dimensions
+    if X.ndim == 1:
+        X = X[None]  # promote to 2-dimensions
+
+    # useful variables
+    m = X.shape[0]
+    num_labels = Theta2.shape[0]
+
+    # You need to return the following variables correctly
+    p = np.zeros(X.shape[0])
+
+    # ====================== YOUR CODE HERE ======================
+    # Add ones to the X data matrix
+    X = np.concatenate([np.ones((m, 1)), X], axis=1)
+    a2 = utils.sigmoid(np.dot(X, np.transpose(Theta1)))
+    a2 = np.concatenate([np.ones((m, 1)), a2], axis=1)
+    a3 = utils.sigmoid(np.dot(a2, np.transpose(Theta2)))
+
+    p = np.argmax(a3, axis=1)
+
+
+    # =============================================================
+    return p
