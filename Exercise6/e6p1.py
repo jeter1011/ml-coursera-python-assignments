4cc2762ba1db22d32b8094d353d2ccf9e61b744b
jeter1011 <erickagudelo123@gmail.com>
2021-12-05 22:31:39 -0500

    ml practice

---
+++ Exercise6/e6p1.py
@@ -0,0 +1,42 @@
+# used for manipulating directory paths
+import os
+
+# Scientific and vector computation for python
+import numpy as np
+
+# Import regular expressions to process emails
+import re
+
+# Plotting library
+from matplotlib import pyplot as plt
+
+# Optimization module in scipy
+from scipy import optimize
+
+# will be used to load MATLAB mat datafile format
+from scipy.io import loadmat
+
+# library written for this exercise providing additional functions for assignment submission, and others
+import utils
+
+
+# Load from ex6data1
+# You will have X, y as keys in the dict data
+data = loadmat(os.path.join('Data', 'ex6data1.mat'))
+X, y = data['X'], data['y'][:, 0]
+
+# Plot training data
+utils.plotData(X, y)
+plt.show()
+
+# You should try to change the C value below and see how the decision
+# boundary varies (e.g., try C = 1000)
+C = 1
+
+model = utils.svmTrain(X, y, C, utils.linearKernel, 1e-3, 20)
+utils.visualizeBoundaryLinear(X, y, model)
+
+
+
+
+
